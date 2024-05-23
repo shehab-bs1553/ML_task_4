3411f0e24349b2832a083f671d841db010814030
Md shehab Hossain <shehab.hossain@brainstation-23.com>
2024-05-24 00:47:45 +0600

    modified

--- Code/main.py
+++ Code/main.py
@@ -46,52 +46,42 @@ def add_noise(image):
 
 output_directory= 'cv_output'
 main_directory = 'Selected_Image_folder'
-# def add_into_file(name):
-#     output_folder_path = os.path.join(output_directory, folder)
-#     os.makedirs(output_folder_path, exist_ok=True)             
-#     file_name, file_extension = os.path.splitext(filename)               
-#     os.path.join("output_file_path_"f"{name}") = os.path.join(output_directory, folder, f"{file_name}_edges{file_extension}")              
-#     cv2.imwrite(output_file_path_edge,name)
+
+def save_into_file(name,folder,filename,Name):
+    output_folder_path = os.path.join(output_directory, folder)
+    os.makedirs(output_folder_path, exist_ok=True)             
+    file_name, file_extension = os.path.splitext(filename)     
+    output_file_path_edge = os.path.join(output_directory, folder, f"{file_name}"+Name+f"{file_extension}")              
+    cv2.imwrite(output_file_path_edge,name)
     
 #Function for doing all the task
 def processing_image(image,filename,folder):
+    
     #code for gray_scale the images and find the edges of the images and also saved into the cv_output file
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)              
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
+    save_into_file(gray_image,folder,filename,"_gray_image")           
     edges = cv2.Canny(gray_image, 100, 200) 
+    save_into_file(gray_image,folder,filename,"_edges_image")
     #code for resize the images and save it
-    resized_image = cv2.resize(image, (225,225))
+    resized_image = cv2.resize(image, (256,256))
+    save_into_file(gray_image,folder,filename,"_resized_image")
     #code for blurring the images and save it
     blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+    save_into_file(gray_image,folder,filename,"_blured_image")
+    
+    
     #code for add noise into the images and save it
     noisy_image = add_noise(image)
+    save_into_file(gray_image,folder,filename,"_noised_image")
     # Perform histogram equalization
     equalized_image = cv2.equalizeHist(gray_image)
+    save_into_file(gray_image,folder,filename,"_histogram_image")
     #add global and adaptive threshold
-    _, global_threshold = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)              
+    _, global_threshold = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    save_into_file(gray_image,folder,filename,"_global_threshold_image")              
     adaptive_threshold = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-                   
+    save_into_file(gray_image,folder,filename,"_adaptive_threshold_image")               
     
-    output_folder_path = os.path.join(output_directory, folder)
-    os.makedirs(output_folder_path, exist_ok=True)             
-    file_name, file_extension = os.path.splitext(filename)               
-    output_file_path_edge = os.path.join(output_directory, folder, f"{file_name}_edges{file_extension}")              
-    cv2.imwrite(output_file_path_edge,edges)
-    output_file_path_gray = os.path.join(output_directory, folder, f"{file_name}_gray_image{file_extension}")              
-    cv2.imwrite(output_file_path_gray,gray_image)
-    output_file_path_resize = os.path.join(output_directory, folder, f"{file_name}_resized_image{file_extension}")              
-    cv2.imwrite(output_file_path_resize,resized_image)
-    
-    output_file_path_blur = os.path.join(output_directory, folder, f"{file_name}_blurred_image{file_extension}")              
-    cv2.imwrite(output_file_path_blur,blurred_image )
-    output_file_path_noise = os.path.join(output_directory, folder, f"{file_name}_noise_image{file_extension}")              
-    cv2.imwrite(output_file_path_noise,noisy_image)
-    output_file_path_hist = os.path.join(output_directory, folder, f"{file_name}_hist_image{file_extension}")              
-    cv2.imwrite(output_file_path_hist,equalized_image)
-    
-    global_output_file_path = os.path.join(output_directory, folder, f"{file_name}_global_threshold{file_extension}")
-    cv2.imwrite(global_output_file_path, global_threshold)
-    output_file_path_adap = os.path.join(output_directory, folder, f"{file_name}_adap_threshold_image{file_extension}")              
-    cv2.imwrite(output_file_path_hist,adaptive_threshold)
     
 #Iterate through all the images using for loop
 
