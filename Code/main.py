5700ba63359e641f419424a7533f61829c985d5e
Md shehab Hossain <shehab.hossain@brainstation-23.com>
2024-05-24 11:04:05 +0600

    modified into main.py to add augmentation

--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,9 @@ import shutil
 import cv2
 import os
 import numpy as np
+import albumentations as A
+from albumentations import RandomBrightnessContrast, HorizontalFlip, Rotate
+
 
 main_directory = 'pokemon_image\Pokemon'
 destination_directory = 'Selected_Image_folder'
@@ -13,6 +16,7 @@ random.seed(42)
 selected_folders = random.sample(all_folders, 10)
 
 
+
 # Select folder randomly from the main folder: 
 def select_folder_randomly():
     for i in selected_folders:
@@ -21,8 +25,8 @@ def select_folder_randomly():
          shutil.copytree(src_path, dst_path)
     print(f"Selected folders and their contents have been copied to {destination_directory}")
     
-#This below should be called only once for selecting the folder randomly
-#after one call this should be comment out otherwise the code gets error 
+#This below call function should be called only once for selecting the folder randomly
+# #after one call this should be comment out otherwise the code gets error 
 
 # select_folder_randomly()
 
@@ -38,6 +42,14 @@ def show(f_path):
                 cv2.waitKey(0) 
                 cv2.destroyAllWindows()
 # show('Selected_Image_folder')
+
+
+augmentations = A.Compose([
+    RandomBrightnessContrast(p=1),
+    HorizontalFlip(p=1),
+    Rotate(limit=90, p=1)
+])
+
 
 output_directory= 'cv_output'
 main_directory = 'Selected_Image_folder'
@@ -56,26 +68,30 @@ def processing_image(image,filename,folder):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
     save_into_file(gray_image,folder,filename,"_gray_image")           
     edges = cv2.Canny(gray_image, 100, 200) 
-    save_into_file(gray_image,folder,filename,"_edges_image")
+    save_into_file(edges,folder,filename,"_edges_image")
     #code for resize the images and save it
     resized_image = cv2.resize(image, (256,256))
-    save_into_file(gray_image,folder,filename,"_resized_image")
+    save_into_file(resized_image,folder,filename,"_resized_image")
     #code for blurring the images and save it
     blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
-    save_into_file(gray_image,folder,filename,"_blured_image")
+    save_into_file(blurred_image,folder,filename,"_blured_image")
     
     
     #code for add noise into the images and save it
     noisy_image = image + np.random.normal(0, 25, image.shape).astype(np.uint8)
-    save_into_file(gray_image,folder,filename,"_noised_image")
+    save_into_file(noisy_image,folder,filename,"_noised_image")
     # Perform histogram equalization
     equalized_image = cv2.equalizeHist(gray_image)
-    save_into_file(gray_image,folder,filename,"_histogram_image")
+    save_into_file(equalized_image,folder,filename,"_histogram_image")
     #add global and adaptive threshold
-    _, global_threshold = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    save_into_file(gray_image,folder,filename,"_global_threshold_image")              
+    ret, global_threshold = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    save_into_file(global_threshold,folder,filename,"_global_threshold_image")              
     adaptive_threshold = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    save_into_file(gray_image,folder,filename,"_adaptive_threshold_image")               
+    save_into_file(adaptive_threshold,folder,filename,"_adaptive_threshold_image")
+    
+    augmented = augmentations(image=image)
+    augmented_image = augmented['image']
+    save_into_file(augmented_image,folder,filename,"_augmented_image")               
     
     
 #Iterate through all the images using for loop for processing the images
